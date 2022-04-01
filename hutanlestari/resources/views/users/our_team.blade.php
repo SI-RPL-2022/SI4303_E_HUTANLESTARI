8e7c2fc9c61241d96763332fb8ab5aa50eb79d3e
VinaFadillah <vinafadillah@student.telkomuniversity.ac.id>
2022-04-01 11:11:54 +0700

    Menambahkan page infokehutanan, page our_team, page layout

---
+++ hutanlestari/resources/views/users/our_team.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('title', 'Our Team')
+
+@section('content')
+    <h1 class="fw-bold text-center mb-4">Our Team</h1>
+    <div class="ms-0 ms-md-5 me-0 me-md-5">
+        <div class="container" align="center">
+            <div class="row">
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Siti Alifah Hartaniah</b></h5>
+                      <p class="card-text">12021</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Regina Tasya Widiharlina</b></h5>
+                      <p class="card-text">12021</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1MYCGU5smusd2LUYHxDpkD45uFa1TtGac" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Vina Fadillah</b></h5>
+                      <p class="card-text">1202194208</p>
+                    </div>
+                </div>
+              </div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Ahmad Dzihan Kamaly</b></h5>
+                      <p class="card-text">12021</p>
+                    </div>
+                </div>
+              </div>
+              <div class="w-100"></div>
+              <div class="col mt-5">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="https://drive.google.com/thumbnail?id=1C07xEjVj1iD1mgCRQIN8-h4xP4-Q-ARJ" alt="Card image cap">
+                    <div class="card-body" style="text-align: left">
+                      <h5 class="card-title"><b>Emas Dai Ashshiddiiqi</b></h5>
+                      <p class="card-text">1202190063</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+@endsection
