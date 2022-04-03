75a7d290385481121ea2af95185c6324312d28ab
reginatasya <reginatasya@student.telkomuniversity.ac.id>
2022-04-03 23:22:07 +0700

    web

--- hutanlestari/routes/web.php
+++ hutanlestari/routes/web.php
@@ -21,4 +21,10 @@ Route::get('opendonasi', function () {
     return view('opendonasi');
 });
 
+<<<<<<< HEAD
 
+=======
+Route::get('LoginRegister', function () {
+    return view('LoginRegister');
+});
+>>>>>>> 05d09bc3d90fea9f7d26e85b73b4fcbeca6da2b8
