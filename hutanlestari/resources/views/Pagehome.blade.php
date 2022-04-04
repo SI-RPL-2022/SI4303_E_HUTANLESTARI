96b143e00e4cef9676b5ffb394c0b5a5c0264322
reginatasya <reginatasya@student.telkomuniversity.ac.id>
2022-04-04 23:25:01 +0700

    menambahkan beberapa isi tampilan pagehome

--- hutanlestari/resources/views/Pagehome.blade.php
+++ hutanlestari/resources/views/Pagehome.blade.php
@@ -13,7 +13,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
   <div class="container-fluid">
-  <img src="C:\xampp\htdocs\cobahutanlestari\logohutan\logo.jpeg" alt="" width="30" height="24">
+  <img src="icon hutan.jpeg" alt="" width="30" height="24">
     <a class="navbar-brand" href="#" > <b>HutanLestari</b></a>
   </div>
   
@@ -45,6 +45,61 @@
   </div>
   
 </nav>
-    
+
+<div class="img img-fluid overflow-hidden" style="max-height: 350px">
+
+    </div>
+<div class="container-fluid bg-card p-3 mb-3">
+    <div class="container">
+        <p class="text-center">Hal sederhana, jadi lebih bermakna bersama Hutan Lestari.
+            Buat aksi penanaman dan lingkungan dengan mudah, aman dan transparan.
+            Mulai <span class="text-success">#CampaginPertama </span>kamu.</p>
+    </div>
+</div>
+
+    <div class="container-fluid p-3 mb-3">
+            <div class="container">
+                <h4 class="text-center text-success mb-3">
+                    Donasi dan Relawan
+                </h4>
+
+                <div class="d-flex justify-content-center">
+                <div class="card w-50 mx-3">
+                    <div class="card-header">
+                        <h5 class="text-success text-center">Donasi</h5>
+                    </div>
+                    <div class="card-body" style="height: 200px">
+
+                    </div>
+                </div>
+
+                <div class="card w-50 mx-3">
+                    <div class="card-header">
+                        <h5 class="text-success text-center">Volunteer</h5>
+                    </div>
+                    <div class="card-body" style="height: 200px">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contaner p-3 mb-3 bg-card">
+        <div class="container row justify-content-center align-items-md-stretch">
+            <div class="col-1"></div>
+            <div class="col-6">
+                <h5>
+                    Ayo ikuti <span class="text-success">#CampaignTerkini</span>
+                    dari HutanLestari yang sedang berjalan saat ini..
+                    Jangan sampai ketinggalan, ya!
+                </h5>
+            </div>
+
+        </div>
+
+    </div>
+
+        
 </body>
 </html>
