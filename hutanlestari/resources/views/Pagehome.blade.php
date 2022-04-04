21f4df0d7eb3f0ca46eb9d5ed925ec861095a313
reginatasya <reginatasya@student.telkomuniversity.ac.id>
2022-04-04 15:06:50 +0700

    menambah isi pagehome

--- hutanlestari/resources/views/Pagehome.blade.php
+++ hutanlestari/resources/views/Pagehome.blade.php
@@ -3,10 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Pagehome</title>
 </head>
+
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+  <div class="container-fluid">
+  <img src="C:\xampp\htdocs\cobahutanlestari\logohutan\logo.jpeg" alt="" width="30" height="24">
+    <a class="navbar-brand" href="#" > <b>HutanLestari</b></a>
+  </div>
+  
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="container ">
+        <ul class="navbar-nav justify-content-center ">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#Home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Campaign">Campaign</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Information">Information</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Blog">Blog</a>
+          </li>
+          <button type="button" class="btn btn-outline-success">Register</button>
+      </div>
+        
+      </ul>
+      
+    </div>
+  </div>
+  
+</nav>
     
 </body>
 </html>
