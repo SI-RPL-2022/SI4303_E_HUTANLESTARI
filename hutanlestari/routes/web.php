c2e00e7bfa9a09c9858cf49c219c8c2f9c4be15e
VinaFadillah <vinafadillah@student.telkomuniversity.ac.id>
2022-04-19 08:01:48 +0700

    perbaikan 3

--- hutanlestari/routes/web.php
+++ hutanlestari/routes/web.php
@@ -30,7 +30,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
 
 Auth::routes();
 
@@ -44,15 +43,3 @@ Route::prefix('campaign')->group(function () {
     Route::post('/donasi/{id}', [\App\Http\Controllers\campaignController::class, 'donasi'])->name('campaign.donasi');
     Route::get('/donasi/{id}', [\App\Http\Controllers\campaignController::class, 'donasipost'])->name('campaign.donasi');
 });
-=======
-Route::prefix('campaign')->group(function (){
-    Route::get('/index' , [\App\Http\Controllers\campaignController::class , 'index'])->name('campaign.index');
-    Route::get('/formcampaign' , [\App\Http\Controllers\campaignController::class , 'form'])->name('campaign.form');
-    Route::post('/formcampaign' , [\App\Http\Controllers\campaignController::class , 'formpost'])->name('campaign.form');
-    Route::get('/detail/{id}' , [\App\Http\Controllers\campaignController::class , 'detail'])->name('campaign.detail');
-    Route::get('/donasi/{id}' , [\App\Http\Controllers\campaignController::class , 'donasi'])->name('campaign.donasi');
-    Route::post('/donasi/{id}' , [\App\Http\Controllers\campaignController::class , 'donasipost'])->name('campaign.donasi');
-
-});
-
->>>>>>> e109a7bd107d262ed314c7eba8ce5d8c4c9caac1
