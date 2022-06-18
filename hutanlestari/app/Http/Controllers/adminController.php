78685bc6e4fa0ceb6c9dca85ca2bf82f1afe4577
VinaFadillah <vinafadillah@student.telkomuniversity.ac.id>
2022-06-18 13:08:26 +0700

    menambahkan controller untuk dashboard admin

--- hutanlestari/app/Http/Controllers/adminController.php
+++ hutanlestari/app/Http/Controllers/adminController.php
@@ -10,11 +10,85 @@ use App\Models\Florafauna;
 use App\Models\InformasiFauna;
 use App\Models\InformasiFlora;
 use App\Models\Volunteer;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class adminController extends Controller
 {
+
+    public function dashboard()
+    {
+        $donasi = count(Campaign::all()->where('donation_check', 1)->where('volunteer_check', 0));
+        $volunteer = count(Campaign::all()->where('donation_check', 0)->where('volunteer_check', 1));
+        $keduanya = count(Campaign::all()->where('donation_check', 1)->where('volunteer_check', 1));
+
+        $don1 = Campaign::all()->where('donation_check', 1)->where('volunteer_check', 0);
+        $donasikelar = 0;
+        foreach ($don1 as $d) {
+            if ($d['target'] <  $d['donasi_terkini']) {
+                $donasikelar += 1;
+            }
+        }
+
+        $vol1 = Campaign::all()->where('donation_check', 0)->where('volunteer_check', 1);
+        $volunteerkelar = 0;
+        foreach ($vol1 as $d) {
+            if ($d['target_volunteer'] <  $d['volunteer_terkini']) {
+                $volunteerkelar += 1;
+            }
+        }
+
+        $sem1 = Campaign::all()->where('donation_check', 1)->where('volunteer_check', 1);
+        $semkelar = 0;
+
+        foreach ($sem1 as $d) {
+            if ($d['target_volunteer'] <  $d['volunteer_terkini'] && $d['target'] <  $d['donasi_terkini']) {
+                $semkelar += 1;
+            }
+        }
+
+        $record = Campaign::select(
+            DB::raw("COUNT(*) as count"),
+            DB::raw("DATE_FORMAT(created_at,'%M %Y') as months")
+        )
+            ->groupBy('months')
+            ->get();
+
+        $data = [];
+
+        foreach ($record as $row) {
+            $data['label'][] = $row->months;
+            $data['data'][] = (int) $row->count;
+        }
+
+        $data['chart_data'] = json_encode($data);
+
+        $recordDonasi = Donasi::select(\DB::raw("COUNT(*) as count"), \DB::raw("DAYNAME(created_at) as day_name"), \DB::raw("DAY(created_at) as day"))
+            ->where('created_at', '>', Carbon::today()->subDay(6))
+            ->groupBy('day_name', 'day')
+            ->orderBy('day')
+            ->get();
+
+        $dataDonasi = [];
+
+        foreach ($recordDonasi as $row) {
+            $dataDonasi['label'][] = $row->day_name;
+            $dataDonasi['data'][] = (int) $row->count;
+        }
+
+        $dataDonasi['chart_data'] = json_encode($dataDonasi);
+
+
+        return view('admin.dashboard', [
+            'donasi' => $donasi, 'volunteer' => $volunteer, 'keduanya' => $keduanya, 'donasikelar' => $donasikelar,
+            'volunteerkelar' => $volunteerkelar, 'semuakelar' => $semkelar, 'data' => $data, 'dataDonasi' => $dataDonasi
+        ]);
+    }
+
+
+
     public function informasifaunaform()
     {
         return view('admin.informasifaunaform');
@@ -157,13 +231,15 @@ class adminController extends Controller
     }
 
     // Verifikasi Donasi Dana
-    public function verifdana(){
-        $data = Donasi::orderBy('created_at' , 'DESC')->get();
-        return view('admin.verifdonasi' , ['data' => $data]);
+    public function verifdana()
+    {
+        $data = Donasi::orderBy('created_at', 'DESC')->get();
+        return view('admin.verifdonasi', ['data' => $data]);
     }
 
-    
-    public function verifdanapost($id){
+
+    public function verifdanapost($id)
+    {
         $data = Donasi::find($id);
         $data->verifikasi_check = 1;
         $data->update();
@@ -173,10 +249,10 @@ class adminController extends Controller
         $ya->update();
 
         return redirect()->back();
-
     }
 
-    public function tolakdana($id){
+    public function tolakdana($id)
+    {
         $data = Donasi::find($id);
         $data->delete();
 
@@ -184,12 +260,14 @@ class adminController extends Controller
     }
 
     //Verifikasi Volunteer yang dibuat
-    public function verifvolun(){
-        $data = Volunteer::orderBy('created_at' , 'DESC')->get();
-        return view('admin.verifvolun' , ['data' => $data]);
+    public function verifvolun()
+    {
+        $data = Volunteer::orderBy('created_at', 'DESC')->get();
+        return view('admin.verifvolun', ['data' => $data]);
     }
 
-    public function verifvolunpost($id){
+    public function verifvolunpost($id)
+    {
 
         $data = Volunteer::find($id);
         $data->verifikasi_check = 1;
@@ -202,7 +280,8 @@ class adminController extends Controller
         return redirect()->back();
     }
 
-    public function tolakvolun($id){
+    public function tolakvolun($id)
+    {
         $data = Volunteer::find($id);
         $data->delete();
         return redirect()->back();
@@ -237,18 +316,21 @@ class adminController extends Controller
 
     // verif donasi dana (emas)
 
-    public function feedback(){
+    public function feedback()
+    {
         $data = Feedback::all();
-        return view('admin.feedback' , ['data'=>$data]);
+        return view('admin.feedback', ['data' => $data]);
     }
 
     // verif blog
-    public function blog(){
-        $data = Blog::orderBy('created_at' , 'DESC')->get();
-        return view('admin.verifblog' , ['data' => $data]);
+    public function blog()
+    {
+        $data = Blog::orderBy('created_at', 'DESC')->get();
+        return view('admin.verifblog', ['data' => $data]);
     }
 
-    public function blogverif($id){
+    public function blogverif($id)
+    {
         $data = Blog::find($id);
         $data->verifikasi_check = 1;
         $data->update();
@@ -256,7 +338,8 @@ class adminController extends Controller
         return redirect()->back();
     }
 
-    public function tolakblog($id){
+    public function tolakblog($id)
+    {
         $data = Blog::find($id);
         $data->verifikasi_check = 1;
         $data->delete();
